5223da98961357ae23889f77f440976d7f0e6e25
vibinsam-008 <vibinappachi@gmail.com>
2026-07-10 17:55:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = [ ]
+name = [ ] # Creating a empty list to store names through append
 i = 0
 def show_menu():
     print("1.Add students")
@@ -7,13 +7,13 @@ def show_menu():
     print("4.Display student")
 def add_students():
     n = input("Enter name")
-    name.append(n)
-    return name
+    name.append(n) #append is used to add the name at the end of the list
+    return name #since name is a string it must be returned 
 
 def search_students():
     s = input("Search for a student name")
-    for i in range(len(name)):
-        if s == name[i]:
+    for i in range(len(name)): #i must lie in the range of total names enrolled and it is true
+        if s == name[i]: #loops till the full initial match with existing ones
             print("Match found", name[i])
             return
         else:
@@ -24,24 +24,24 @@ def remove_students():
     r = input("Enter the student to be removed")
     for i in range(len(name)):
         if r == name[i]:
-            name.remove(r)
+            name.remove(r) #removes a student, no matter the position it holds in the list
             print("Removed")
             break
 
 def display_students():
     for i in range(len(name)):
         print("Student Name:", name[i])
-menu = show_menu()
+menu = show_menu() #shows the menu
 while True:
-    d = int(input("Enter the decision"))
+    d = int(input("Enter the decision")) #asks for the tab to be redirected to
 
-    if d == 1:
-        add_students()
+    if d == 1: 
+        add_students() #calls add student function
     elif d == 2:
-        search_students()
+        search_students()#calls search student function
     elif d == 3:
-        remove_students()
+        remove_students() # calls remove student function
     elif d == 4:
-        display_students()
+        display_students() # calls display student function
     else:
         print("Invalid")
