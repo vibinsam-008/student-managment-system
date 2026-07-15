f0e93fa33fc0c7c8a1269d4adeba1b080a9c11d0
Vibin Appachi <vibinappachi@gmail.com>
2026-07-15 20:14:57 +0530

    Create remove.py

---
+++ remove.py
@@ -0,0 +1,8 @@
+from data import dat
+def rem():
+  r = input("Enter the student to be removed")
+  for name in dat:
+    if data["name"] == r:
+      data.remove(data)
+      print("student", r, "removed")
+    
