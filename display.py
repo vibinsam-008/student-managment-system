1ceb5923393bf6996274bbfa471d8dc8e6f50b3d
Vibin Appachi <vibinappachi@gmail.com>
2026-07-15 20:22:23 +0530

    Create display.py

---
+++ display.py
@@ -0,0 +1,8 @@
+from data import dat
+def dis():
+  for name in dat:
+    print("Name:  ", data["name"])
+    print("Date of birth:  ", data["DOB"])
+    print("School name:  ", data["school"])
+    print("Average:  ", data["avg"])
+    
